bee9ec977497cdddb4455c31a42c3804f08ceab2
Soumyadip Maity <maity.alpha@gmail.com>
2026-06-08 12:17:43 +0530

    Arrays

---
+++ Arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <cmath> 
+
+using namespace std;
+
+int main()
+
+{
+
+    int luckynumbers[] = {4, 8, 15, 16, 23, 42}; 
+    int list[20]; 
+    list[0] = 5; 
+
+    cout << luckynumbers[0] << endl;
+
+    luckynumbers[0] = 19;  
+    cout << luckynumbers[0]; 
+
+    return 0;
+}
