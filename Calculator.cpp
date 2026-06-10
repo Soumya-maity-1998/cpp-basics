33ca382db70acdcab6260bd83599fc11eb0d4a2c
Soumyadip Maity <maity.alpha@gmail.com>
2026-06-10 11:37:04 +0530

    Complete calculator.

--- Calculator.cpp
+++ Calculator.cpp
@@ -7,13 +7,29 @@ int main()
 
 {
     int num1, num2; 
+    char op; 
+
     cout << "Enter first number: ";
     cin >> num1;
 
-    cout << "Enter Second number: ";
+    cout << "Enter operator: ";
+    cin >> op; 
+
+    cout << "Enter second number: ";
     cin >> num2; 
 
-    cout << (num1 + num2); 
-
+    int result; 
+    if (op == '+'){
+        result = num1 + num2; 
+    } else if (op == '-') {
+        result = num1 - num2; 
+    } else if (op == '*') {
+        result = num1*num2; 
+    } else if (op == '/') {
+        result = num1/num2; 
+    } else {
+        cout << "Invalid Operator.";
+    }
+    cout << result; 
     return 0;
 }
